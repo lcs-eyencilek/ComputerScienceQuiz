1d8af72e4f4c38eceab728e21770025ae90da177
lcs-eyencilek <90844648+lcs-eyencilek@users.noreply.github.com>
2022-03-07 08:50:36 +0300

    Initial Commit

---
+++ Shared/ComputerScienceQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputerScienceQuizApp.swift
+//  Shared
+//
+//  Created by Efe Yencilek on 2022-03-07.
+//
+
+import SwiftUI
+
+@main
+struct ComputerScienceQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
